3b1c845f8e266895468bab6f2696816872ca6cca
ipatini <ipatini@mail.ntua.gr>
2023-08-20 14:01:47 +0300

    Nebulous: Updated License notice in EMS Nebulous plugin

--- src/main/java/eu/nebulous/ems/translate/NebulousEmsTranslatorProperties.java
+++ src/main/java/eu/nebulous/ems/translate/NebulousEmsTranslatorProperties.java
@@ -1,8 +1,7 @@
 /*
- * Copyright (C) 2017-2023 Institute of Communication and Computer Systems (imu.iccs.gr)
+ * Copyright (C) 2023-2025 Institute of Communication and Computer Systems (imu.iccs.gr)
  *
- * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0, unless
- * Esper library is used, in which case it is subject to the terms of General Public License v2.0.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
  * If a copy of the MPL was not distributed with this file, you can obtain one at
  * https://www.mozilla.org/en-US/MPL/2.0/
  */
