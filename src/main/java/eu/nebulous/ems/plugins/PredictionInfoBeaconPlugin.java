7c04f340832d2b13be0c82b90ab4f0cb618c37d5
ipatini <ipatini@mail.ntua.gr>
2023-12-12 14:42:28 +0200

    Nebulous: Upgraded to SB 3.2.0 to match EMS core

--- src/main/java/eu/nebulous/ems/plugins/PredictionInfoBeaconPlugin.java
+++ src/main/java/eu/nebulous/ems/plugins/PredictionInfoBeaconPlugin.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import javax.jms.JMSException;
+import jakarta.jms.JMSException;
 import java.util.Collections;
 import java.util.Set;
 
